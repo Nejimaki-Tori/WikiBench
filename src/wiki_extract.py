755732048b31a6cd72da90408f52351a09bb7afd
GrigorevDA <dagrig14@yandex.ru>
2025-03-12 01:14:11 +0300

    Добавил сохранение текста из источников

--- src/wiki_extract.py
+++ src/wiki_extract.py
@@ -15,37 +15,40 @@ class Extracter(WikiParser):
         for source_key, items in tqdm((self.links).items()):
             extracted_links = []
             for item1 in items:
-              item = item1[0]
-              if item.startswith("http"):
-                try:
-                  response = requests.get(item, headers=self.headers, stream=True, allow_redirects=False, timeout=(3, 7))
-                  response.raise_for_status()
-                  text = response.text
-                  if text:
-                    extracted_links.append(text)
-                except requests.RequestException as e:
-                  continue
+                item = item1[0]
+                if item.startswith("http"):
+                    try:
+                        response = requests.get(item, headers=self.headers, stream=True, allow_redirects=False, timeout=(3, 7))
+                        response.raise_for_status()
+                        text = response.text
+                        if text:
+                            extracted_links.append(text)
+                    except requests.RequestException as e:
+                        continue
             if extracted_links:
-              links_by_sources[source_key] = extracted_links
+                links_by_sources[source_key] = extracted_links
         self.html_text = links_by_sources
     
     
     def newspaper_ref(self):
         ref_texts = {}
+        max_attempts = 2
         for source_key, items in tqdm((self.links).items()):
             extracted_links = []
             for item1 in items:
-              item = item1[0]
-              if item.startswith("http"):
-                try:
-                  art = Article(item, language='ru')
-                  art.download()
-                  art.parse()
-                  text = art.text
-                  if text:
-                    extracted_links.append(text)
-                except:
-                  continue
+                item = item1[0]
+                if item.startswith("http"):
+                    for attempt in range(1, max_attempts + 1):
+                        try:
+                            art = Article(item, language='ru')
+                            art.download()
+                            art.parse()
+                            text = art.text
+                            if text:
+                                extracted_links.append(text)
+                            break
+                        except:
+                            continue
             if extracted_links:
               ref_texts[source_key] = extracted_links
         self.ref_texts = ref_texts
